a10cc2b5e895ef9a2bf9b01891649907efe7bf4e
kumardinesh98 <devadineshkumar98@gmail.com>
2021-12-13 21:52:46 -0800

    naumber of days profit or loss is added with amount

--- com/gamble/Gambling.java
+++ com/gamble/Gambling.java
@@ -1,30 +1,55 @@
 package com.gamble;
 
 public class Gambling {
-	static int stake = 100;
+	static long stake = 100;
 	static int day = 1;
-	static int target =stake+ (stake/2);
-	static int loss = (stake/2);
+	static int month = 1;
+	static int  daysWon = 0;
+	static int daysLost = 0;
+	static int temp ;
+	static long thisMonth;
+	static long daysprofit;
+	static long target =stake+ (stake/2);
+	static long loss = (stake/2);
 	
 	public static void main(String[] args) {
 		Gambling gambling = new Gambling();
-		while (day <= 20) {
-		
-			while(stake > loss && stake < target ) {
-				int temp = gambling.random();
-				if (temp == 0) {
-					stake = stake+2; 
+		while (month <= 2) {
+			thisMonth = stake;
+			while (day <= 20) {
+				daysprofit = stake;
+				while(stake > loss && stake < target ) {
+					temp = gambling.random();
+					if (temp == 0) {
+						stake = stake+1; 	
+					}
+					else {
+						stake = stake-1;
+						
+					}
+					
+				}
+				if ((stake-daysprofit) > 0) {
+					daysWon += 1;
+					System.out.println("day "+day+" profit  "+stake);
 				}
 				else {
-					stake = stake-1;
+					daysLost += 1;
+					System.out.println("day "+day+" loss  "+stake);
 				}
+				
+			
+				stake = stake + 100;
+				target = stake +(stake/2);
+				loss = (stake/2);
+				day +=1;
 			}
-			System.out.println("day "+day+" P/L  "+stake);
-		
-			stake = stake + 100;
-			target = stake +(stake/2);
-			loss = (stake/2);
-			day +=1;
+			long monthPandL = stake - thisMonth;
+			System.out.println("days won = "+ daysWon);
+			System.out.println("days lost = "+ daysLost);
+			System.out.println("month" + month + "P/L is  " + monthPandL );
+			day =1;
+			month ++;
 		}
 		
 		
