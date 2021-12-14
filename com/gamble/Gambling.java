9c011a61258ba45220674df59e62551f0651185c
kumardinesh98 <devadineshkumar98@gmail.com>
2021-12-13 20:16:40 -0800

    progarm to print daily profit or loss

--- com/gamble/Gambling.java
+++ com/gamble/Gambling.java
@@ -2,29 +2,35 @@ package com.gamble;
 
 public class Gambling {
 	static int stake = 100;
-	int day = 1;
+	static int day = 1;
 	static int target =stake+ (stake/2);
 	static int loss = (stake/2);
 	
 	public static void main(String[] args) {
 		Gambling gambling = new Gambling();
-		while(stake > loss && stake < target ) {
-			gambling.game(gambling.random());
-			System.out.println(stake);
+		while (day <= 20) {
+		
+			while(stake > loss && stake < target ) {
+				int temp = gambling.random();
+				if (temp == 0) {
+					stake = stake+2; 
+				}
+				else {
+					stake = stake-1;
+				}
+			}
+			System.out.println("day "+day+" P/L  "+stake);
+		
+			stake = stake + 100;
+			target = stake +(stake/2);
+			loss = (stake/2);
+			day +=1;
 		}
+		
 		
 	}
 	public int random() {
 		return (int)(Math.floor(Math.random()*10) % 2 );
 	}
-	public void game(int random) {
-		switch (random) {
-		case 0:
-			stake = stake+1;
-			break;
-		case 1:
-			stake = stake-1;
-		}
-	}
+	
 }
-
